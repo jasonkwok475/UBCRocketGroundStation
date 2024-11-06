9fbcfe6c256562fcfca0815f8615d43762b7e123
Jason <jasonkwok375@gmail.com>
2024-11-05 16:05:34 -0800

    Update build.py

--- build.py
+++ build.py
@@ -126,7 +126,7 @@ def build_step():
     hidden_imports = [f"--hidden-import={i}" for i in HIDDEN_IMPORTS]
     data_files = [f"--add-data={i}{PYINSTALLER_SEPARATOR}{parent_dir(i)}" for i in DATA_FILES]
     splash = "" #f"--splash {SPLASH_IMAGE}" if sys.platform != 'darwin' else ""  # Splash not currently supported on MacOS
-    _run(VENV_PYINSTALLER, ['--onefile',
+    _run(VENV_PYINSTALLER, ['--onefile', 
                             ENTRY_POINT,
                             '--console',
                             '--name', EXECUTABLE_NAME,
