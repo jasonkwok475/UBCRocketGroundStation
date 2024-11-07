a8ec3ab57fe8e96da76327f630d008fe30cc074e
Jason <jasonkwok375@gmail.com>
2024-11-07 08:59:59 -0800

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -9,7 +9,7 @@ set -o verbose
 
 sudo apt-get update -y
 
-sudo apt-get install tcl tk tk-dev tk-devel python3-tk python3.12-tk
+sudo apt-get install tcl tk tk-dev python3-tk 
 python -m tkinter
 
 # Install missing packages for qt - libxcb-iccm4.so not found
